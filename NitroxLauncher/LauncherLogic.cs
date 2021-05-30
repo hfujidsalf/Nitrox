8a3dc4f5899710a4f6c308cc364fd5e13da033e1
hfujidsalf <85063335+hfujidsalf@users.noreply.github.com>
2021-05-30 15:23:17 +0300

    Update LauncherLogic.cs

--- NitroxLauncher/LauncherLogic.cs
+++ NitroxLauncher/LauncherLogic.cs
@@ -289,7 +289,7 @@ namespace NitroxLauncher
                     break;
 
                 case Platform.STEAM:
-                    startInfo.FileName = "steam://run/264710";
+                    startInfo.FileName = "D:\Games\Subnautica\Subnautica.exe";
                     break;
 
                 case Platform.MICROSOFT:
